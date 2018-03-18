074e36ce59c5a2b8414771cb715538cb992353fd
小黑黑 <@DESKTOP-LSUBA5R>
2018-03-18 17:51:21 +0800

    test

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -3,7 +3,7 @@ package helloworld;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("HEllO WORLD!");
+		System.out.println(" ÐÞ¸ÄºóµÄHEllO WORLD!");
 	}
 	
 }
